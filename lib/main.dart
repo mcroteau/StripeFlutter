99c8d73685c115a67796174f3d6e52244952b85e
Mike Croteau <croteau.mike@gmail.com>
2021-02-04 19:49:07 -0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:dio/dio.dart';
+import 'package:StripeFlutter/z.dart' as z;
 
 void main() {
   runApp(MyApp());
@@ -56,27 +56,27 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  String planId;
+  String tokenId;
+  String customerId;
+  String subscriptionId;
+
   bool processing = false;
 
-  var token;
-  var customer;
-  var product;
-  var plan;
-
-  final String STRIPE_KEY = "";
   final String STRIPE_URL = "https://api.stripe.com/v1/";
 
   final String CUSTOMERS_ENDPOINT = "customers";
   final String PRODUCTS_ENDPOINT = "products";
   final String PLANS_ENDPOINT = "plans";
   final String TOKENS_ENDPOINT = "tokens";
+  final String SUBSCRIPTIONS_ENDPOINT = "subscriptions";
 
 
   TextEditingController email = new TextEditingController();
   TextEditingController creditCard = new TextEditingController();
   TextEditingController expMonth = new TextEditingController();
   TextEditingController expYear = new TextEditingController();
-  TextEditingController cvv = new TextEditingController();
+  TextEditingController cvc = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -91,42 +91,147 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
               RaisedButton(
-                  child: Text("Generate Plans"),
-                  color: Colors.lightBlue,
+                  child: Text("Generate Plan"),
+                  color: Colors.blue,
                   textColor: Colors.white,
                   onPressed: save
               ),
               RaisedButton(
-                  child: Text("Delete All Plans"),
-                  color: Colors.redAccent,
+                  child: Text("Subscribe"),
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  onPressed: subscribe
+              ),
+              RaisedButton(
+                  child: Text("Cancel Subscription"),
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  onPressed: cancel
+              ),
+              RaisedButton(
+                  child: Text("Delete Plans"),
+                  color: Colors.red,
                   textColor: Colors.white,
                   onPressed: delete
-              )
+              ),
             ]
           )
        )
       );
   }
 
+
+  Future cancel() async{
+    if(!processing){
+      setProcessing(true);
+      Map<String, String> body = new HashMap();
+      sendReq(SUBSCRIPTIONS_ENDPOINT + "/" + subscriptionId, 'delete', body).then((data) => displayCanceled(data));
+    }
+  }
+
+  void displayCanceled(data){
+    var json = jsonDecode(data);
+    String content = "Successfully canceled @ " + json['canceled_at'].toString();
+    showGlobalDialogNoOkay(content, null);
+    setProcessing(false);
+  }
+
+
+  Future subscribe() async{
+    if (!processing) {
+      setProcessing(true);
+      createToken()
+          .then((value) => createCustomer(value))
+          .then((customerData) => createSubscription(customerData));
+    }
+  }
+
+  Future createToken() async {
+    Map<String, String> body = new HashMap();
+    body['card[number]'] = "4242424242424242";
+    body['card[exp_month]'] = "10";
+    body['card[exp_year]'] = "2023";
+    body['card[cvc]'] = "123";
+    return sendReq(TOKENS_ENDPOINT, 'post', body);
+  }
+
+  Future createCustomer(value) async {
+    var token = jsonDecode(value);
+    print("token " + token['id']);
+    tokenId = token['id'];
+    Map<String, String> body = new HashMap();
+    body['email'] = "croteau.mike@gmail.com";
+    body['source'] = token['id'];
+    return sendReq(CUSTOMERS_ENDPOINT, 'post', body);
+  }
+/*
+cus_It8ERSRb4mqhSj
+plan_It8CpRuJWL56O1
+ */
+  Future createSubscription(customerData){
+    var json = jsonDecode(customerData);
+    customerId = json['id'];
+    Map<String, String> body = new HashMap();
+    body['customer'] = json['id'];;
+    body['items[0][plan]'] = planId;
+    sendReq(SUBSCRIPTIONS_ENDPOINT, 'post', body)
+        .then((data) => displayData(data));
+  }
+
+  void displayData(data){
+    var json = jsonDecode(data);
+    subscriptionId = json['id'];//what you will want to store to delete later.
+    String content = "customer id: " + customerId + "\n" +
+                      "subscription id : " + json['id'];
+    showGlobalDialogNoOkay(content, null);
+    setProcessing(false);
+  }
+
+
   Future save() async {
     print("processing");
 
     if (!processing) {
-      setState(() {
-        processing = true;
-      });
-
-      showGlobalDialogNoOkay("Processing...", null);
-      var f = createProduct().then((data) => createPlan(data));
-      print("f : " + f.toString());
+      setProcessing(true);
+      createProduct()
+          .then((data) => createPlan(data))
+          .then((data) => setPlanId(data));
     }
   }
 
+  void setPlanId(data){
+    print("data: " + data);
+    var json = jsonDecode(data);
+    planId = json['id'];
+    setProcessing(false);
+  }
+
+
+  Future createProduct() async {
+    Map<String, String> body = new HashMap();
+    body['name'] = "MockProduct";
+    return sendReq(PRODUCTS_ENDPOINT, 'post', body);
+  }
+
+  Future createPlan(data) async {
+    print("create plan" + data);
+    var json = jsonDecode(data);
+    Map<String, String> body = new HashMap();
+    body['amount'] = "500";
+    body['interval'] = "month";
+    body['currency'] = "usd";
+    body['product'] = json['id'];
+    return sendReq(PLANS_ENDPOINT, 'post', body);
+  }
+
   Future delete() async{
-    getPlans()
-        .then((data) => deletePlans(data))
-        .then((data) => getProducts(data))
-        .then((data) => deleteProducts(data));
+    if (!processing) {
+      setProcessing(true);
+      getPlans()
+          .then((data) => deletePlans(data))
+          .then((data) => getProducts(data))
+          .then((data) => deleteProducts(data));
+    }
   }
 
   Future getPlans() async{
@@ -150,6 +255,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Map<String, String> body = new HashMap();
     return sendReq(PRODUCTS_ENDPOINT, 'get', body);
   }
+
   Future deleteProducts(data) async{
     var products = jsonDecode(data);
     print("data" + products.toString());
@@ -159,31 +265,17 @@ class _MyHomePageState extends State<MyHomePage> {
       Map<String, String> body = new HashMap();
       sendReq(PRODUCTS_ENDPOINT + "/" + name, 'delete', body);
     }
+
+    setProcessing(false);
   }
 
-  Future createProduct() async {
-    Map<String, String> body = new HashMap();
-    body['name'] = "MockProduct";
-    return sendReq(PRODUCTS_ENDPOINT, 'post', body);
-  }
-
-  Future createPlan(data) async {
-    print("create plan" + data);
-    var json = jsonDecode(data);
-    Map<String, String> body = new HashMap();
-    body['amount'] = "500";
-    body['interval'] = "month";
-    body['currency'] = "usd";
-    body['product'] = json['id'];
-    return sendReq(PLANS_ENDPOINT, 'post', body);
-  }
 
 
   Future sendReq(endpoint, method, body) async{
     var respBody;
     try {
       var req = http.Request(method, Uri.parse(STRIPE_URL + endpoint));
-      req.headers['Authorization'] = "Bearer " + STRIPE_KEY;
+      req.headers['Authorization'] = "Bearer " + z.STRIPE_KEY;
       req.headers['Content-Type'] = "application/x-www-form-urlencoded; charset=UTF-8";
       req.bodyFields = body;
 
@@ -204,11 +296,18 @@ class _MyHomePageState extends State<MyHomePage> {
         builder: (BuildContext context) {
           // return object of type Dialog
           return AlertDialog(
-            title: new Text("Message"),
-            content: new Text(content),
+            title: new Text("Result"),
+            content: new Text(content)
           );
         }
     );
+  }
+
+  bool setProcessing(b){
+    setState(() {
+      processing = b;
+    });
+    return true;
   }
 
 }
